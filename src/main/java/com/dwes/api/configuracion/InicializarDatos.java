14f522bc260bcb292f6a6a143b9dd9e7883e6f47
csanrom1702 <147594222+csanrom1702@users.noreply.github.com>
2024-01-12 10:03:42 +0100

    Add files via upload

--- src/main/java/com/dwes/api/configuracion/InicializarDatos.java
+++ src/main/java/com/dwes/api/configuracion/InicializarDatos.java
@@ -11,95 +11,10 @@ import org.springframework.stereotype.Component;
 import com.dwes.api.entidades.Categoria;
 import com.dwes.api.entidades.Ingrediente;
 import com.dwes.api.entidades.Jabon;
-import com.dwes.api.entidades.Producto;
 import com.dwes.api.entidades.enumerados.TipoDePiel;
 import com.dwes.api.repositorios.CategoriaRepository;
 import com.dwes.api.repositorios.JabonRepository;
-import com.dwes.api.repositorios.ProductoRepository;
 import com.github.javafaker.Faker;
-
-/*@Component
-public class InicializarDatos implements CommandLineRunner {
-
-	@Autowired
-	private JabonRepository jabonRepository;
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	Faker faker = new Faker();
-
-	@Override
-    public void run(String... args) throws Exception {
-
-        // Inicializar Categoría
-        Categoria categoria = generarCategoriaAleatoria();
-        categoriaRepository.save(categoria);
-
-        for (int i = 0; i < 100; i++) {
-            Jabon jabon = generarJabonAleatorio();
-            
-            // Asignar la categoría a cada producto
-            jabon.setCategorias(categoria);
-            
-            jabonRepository.save(jabon);
-        }
-    }
-
-    private Jabon generarJabonAleatorio() {
-        Jabon jabon = new Jabon();
-        jabon.setNombre(faker.commerce().productName());
-        double precio = Double.parseDouble(faker.commerce().price().replaceAll("[^\\d.]+", ""));
-        jabon.setPrecio(precio <= 0 ? 1.0 : precio);
-        jabon.setDescripcion(faker.lorem().sentence());
-        jabon.setStock(faker.number().numberBetween(0, 100));
-        jabon.setImagenUrl(generarUrlImagenAleatoria());
-        jabon.setAroma(faker.lorem().word());
-        jabon.setTipoDePiel(TipoDePiel.values()[faker.random().nextInt(TipoDePiel.values().length)]);
-
-        List<Ingrediente> ingredientes = new ArrayList<>();
-        for (int j = 0; j < faker.number().numberBetween(1, 5); j++) {
-            ingredientes.add(generarIngredienteFicticio(faker));
-        }
-        jabon.setIngredientes(ingredientes);
-
-        return jabon;
-    }
-
-    private Categoria generarCategoriaAleatoria() {
-        Categoria categoria = new Categoria();
-        categoria.setNombre(faker.commerce().department());
-        categoria.setDescripcion(faker.lorem().sentence());
-
-        return categoria;
-    }
-
-    private Ingrediente generarIngredienteFicticio(Faker faker) {
-        Ingrediente ingrediente = new Ingrediente();
-        String[] elementos = {"jabón de glicerina", "gel aloe vera", "miel", "aceite de oliva", "ralladura de limón", "aceite esencial"};
-        String[] cantidades = {"2 pastillas", "1 taza", "4 cucharadas", "5 cucharadas", "1 cucharada"};
-
-        // Elegir aleatoriamente un elemento y una cantidad
-        String elemento = elementos[faker.random().nextInt(elementos.length)];
-        String cantidad = cantidades[faker.random().nextInt(cantidades.length)];
-
-        // Si el elemento es "gel aloe vera" o "aceite de oliva", añadir la medida
-        if (elemento.equals("gel aloe vera")) {
-            cantidad += " (200 g)";
-        } else if (elemento.equals("aceite de oliva")) {
-            cantidad += " (100 ml)";
-        }
-
-        ingrediente.setElemento(elemento);
-        ingrediente.setCantidad(cantidad);
-
-        return ingrediente;
-    }
-
-    private String generarUrlImagenAleatoria() {
-        return "https://e00-telva.uecdn.es/assets/multimedia/imagenes/2019/11/08/15732087888279.jpg";
-    }
-}*/
 
 @Component
 public class InicializarDatos implements CommandLineRunner {
@@ -115,17 +30,16 @@ public class InicializarDatos implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Inicializar Categoría
-        Categoria categoria = generarCategoriaAleatoria();
-        categoriaRepository.save(categoria);
+        // Inicializar Categorías
+        for (int i = 0; i < 10; i++) {
+            Categoria categoria = generarCategoriaAleatoria();
+            categoriaRepository.save(categoria);
 
-        for (int i = 0; i < 100; i++) {
-            Jabon jabon = generarJabonAleatorio();
-
-            // Asignar la categoría a cada jabón
-            jabon.getCategorias().add(categoria);
-
-            jabonRepository.save(jabon);
+            for (int j = 0; j < 10; j++) { // Asignar 10 productos a cada categoría
+                Jabon jabon = generarJabonAleatorio();
+                jabon.getCategorias().add(categoria);
+                jabonRepository.save(jabon);
+            }
         }
     }
 
